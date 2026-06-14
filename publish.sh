66c693dbb1be3d37441e2078249c0c4e5ab67657
Kimu1109 <shonosuke1309@outlook.jp>
2026-06-14 16:13:02 +0900

    Append README and LICENSE to releases.

--- publish.sh
+++ publish.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+echo "WELCOME TO PUBLISH SCRIPT!"
+echo "win-x64, win-x64-selfcontained, linux-x64 and linux-x64-selfcontained will be built."
+echo "================================"
+
 cd reversi-evaluation
+
+echo "REMOVE Publish folder"
+rm -r bin/Publish
 
 echo "BUILD win-x64"
 
@@ -36,6 +43,26 @@ dotnet publish \
     -p:PublishSingleFile=true \
     -o bin/Publish/linux-x64-selfcontained
 
+echo "COPY LICENSE & README to win-x64"
+cp ../LICENSE bin/Publish/win-x64
+cp ../LICENSE.edax bin/Publish/win-x64
+cp ../README.md bin/Publish/win-x64
+
+echo "COPY LICENSE & README to linux-x64"
+cp ../LICENSE bin/Publish/linux-x64
+cp ../LICENSE.edax bin/Publish/linux-x64
+cp ../README.md bin/Publish/linux-x64
+
+echo "COPY LICENSE & README to win-x64-selfcontained"
+cp ../LICENSE bin/Publish/win-x64-selfcontained
+cp ../LICENSE.edax bin/Publish/win-x64-selfcontained
+cp ../README.md bin/Publish/win-x64-selfcontained
+
+echo "COPY LICENSE & README to linux-x64-selfcontained"
+cp ../LICENSE bin/Publish/linux-x64-selfcontained
+cp ../LICENSE.edax bin/Publish/linux-x64-selfcontained
+cp ../README.md bin/Publish/linux-x64-selfcontained
+
 cd bin/Publish
 
 echo "ZIP win-x64"
@@ -49,3 +76,5 @@ zip -r win-x64-selfcontained.zip win-x64-selfcontained
 
 echo "TAR.GZ linux-64-selfcontained"
 tar -zcvf linux-64-selfcontained.tar.gz linux-x64-selfcontained
+
+echo "FINISH!"
